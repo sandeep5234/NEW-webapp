8c409d9611b36de660c02db77abc7b36424fa18d
sandeep5234 <sahota2sandeep@yahoo.co.in>
2020-07-12 16:11:27 +1000

    Add project files.

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,46 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace VidlyMaterialize
+{
+    public class BundleConfig
+    {
+        // Per altre informazioni sulla creazione di bundle, vedere https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Utilizzare la versione di sviluppo di Modernizr per eseguire attività di sviluppo e formazione. Successivamente, quando si è
+            // pronti per passare alla produzione, usare lo strumento di compilazione disponibile all'indirizzo https://modernizr.com per selezionare solo i test necessari.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                      "~/Content/bootstrap.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/materialize").Include(
+                      "~/Content/materialize/css/materialize.min.css",
+                      "~/Content/materialize/css/materialize-custom.css"));
+
+            bundles.Add(new StyleBundle("~/Content/sidebar-fixed").Include(
+                      "~/Content/materialize/css/sidebar-fixed.css"));
+
+            bundles.Add(new StyleBundle("~/Content/nouislider").Include(
+                      "~/Content/materialize/nouislider/css/nouislider.min.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/materialize").Include(
+                      "~/Content/materialize/js/materialize.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/nouislider").Include(
+                      "~/Content/materialize/nouislider/js/nouislider.min.js"));
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VidlyMaterialize.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public DateTime? Birthdate { get; set; }
+
+        [Required]
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        [Required]
+        [Display(Name = "Gender")]
+        public byte GenderId { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
